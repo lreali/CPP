0706b395c6bd90e3fc2a75079700c5e7e4583c15
Lara Reali <realielsa@gmail.com>
2022-03-23 00:48:48 +0100

    Sed

---
+++ CPP01/ex04/main.cpp
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ereali <ereali@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 23:48:26 by ereali            #+#    #+#             */
+/*   Updated: 2022/03/23 00:40:41 by ereali           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string	ft_replace(std::string buff, std::string to_replace, std::string to_add)
+{
+	std::size_t pos;
+
+	while ((pos = buff.find(to_replace)) != std::string::npos)
+	{
+		buff.erase(pos, to_replace.size());
+		buff.insert(pos, to_add);
+	}
+	return (buff);
+}
+
+int main(int argc, char **argv)
+{
+	std::string buff;
+	std::string line;
+	std::string name;
+
+	if (argc != 4)
+	{
+			std::cout << "Please enter <filename> <to replace> <new string>" << std::endl;
+			return (1);
+	}
+	if (argv[2][0] == '\0')
+		return (std::cout << "Please enter <filename> <a non empty to_replace> <new string>" << std::endl, 1);
+	std::ifstream ifs(argv[1]);
+	if (ifs.fail())
+	{
+		std::cout << "Please enter <an existing filename> <to replace> <new string>" << std::endl;
+		return (1);
+	}
+	while (std::getline(ifs, line))
+			buff += line + '\n';
+	if (!ifs.eof())
+	{
+		std::cout << "Reading Error" << std::endl;
+		return (1);
+	}
+	ifs.close();
+	buff = ft_replace(buff, argv[2], argv[3]);
+	name = argv[1];
+	name += ".replace";
+	std::ofstream ofs(name);
+	if(ofs.fail())
+		return(std::cout << "Create new file Faillure" << std::endl, 1);
+	ofs << buff;
+	ofs.close();
+	return (0);
+}
