84cdd2ab714c5420c311edc7b9097360318ada0e
Lara Reali <realielsa@gmail.com>
2022-04-13 21:28:38 +0200

    ex00 git push

--- CPP07/ex00/main.cpp
+++ CPP07/ex00/main.cpp
@@ -20,28 +20,28 @@ private :
 };
 
 std::ostream & operator<<(std::ostream & o, const Awesome &a){o << a.get_n(); return o;}
-int main(void)
-{
-	Awesome a(2), b(4);
-	swap(a,b);
-	std::cout << a << " " << b << std::endl;
-	std::cout << max(a,b) << '\n';
-	return(0);
-}
-
-//
-// int main()
+// int main(void)
 // {
-// 	int a = 2;
-// 	int b = 3;
-// 	::swap(a, b);
-// 	std::cout << "a = " << a << ", b = " << b << std::endl;
-// 	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
-// 	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
-// 	std::string c = "chaine1";
-// 	std::string d = "chaine2";
-// 	::swap(c, d);
-// 	std::cout << "c = " << c << ", d = " << d << std::endl;
-// 	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
-// 	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+// 	Awesome a(2), b(4);
+// 	swap(a,b);
+// 	std::cout << a << " " << b << std::endl;
+// 	std::cout << max(a,b) << '\n';
+// 	return(0);
 // }
+
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+}
